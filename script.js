f25603759213cfcbc60d0224a64cf43b48e90539
WLonfat <196331955+WLonfat@users.noreply.github.com>
2025-04-24 00:56:50 +0200

    Finalisation du CSS de la page Contact (+ responsive), corrections de fautes de frappes (FAQ, Mentions-Légales), Ajout du CSS pour les boutons du site, et améloiration des fonctions JS pour le menu burger

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 /*
 Auteur      : William.L
 Version     : 2.0
-Date        : 22.04.2025
+Date        : 23.04.2025
 Description : Scripts du site Lonfdrone
 */
 
-/* Script permettant de rajouter ou supprimer la classe active de menuBurger, pour pouvoir l'ouvrir ou le fermer via le CSS.*/
 
-    var menuBurger = document.getElementById("menuBurger");
-    var buttonOuvrir = document.getElementById("buttonOuvrir");
-    var buttonFermer = document.getElementById("buttonFermer");
+/* Fonctions permettant de rajouter ou supprimer la classe active de menuBurger, pour pouvoir l'ouvrir ou le fermer via le CSS.*/
 
-    buttonOuvrir.onclick = ouvrirNav;
-    buttonFermer.onclick = fermerNav;
+/* Ajout de la classe active à menuBurger */
+function ouvrirNav() {
+    menuBurger.classList.add("active");
+}
 
-    /* Fonction pour ajouter la classe active à menuBurger. (Aligné à 0px à droite)*/
-    function ouvrirNav() {
-        menuBurger.classList.add("active");
-    }
-
-    /* Fonction pour retirer la classe active à menuBurger. (Aligné à -255px à droite)*/
-    function fermerNav() {
-        menuBurger.classList.remove("active");
-    }
+/* Retire la classe active à menuBurger */
+function fermerNav() {
+    menuBurger.classList.remove("active");
+}
 
 /* Fonction pour vérifier les conditions du formulaire de contact*/
 
     function Message() {
 
-        const verification = document.getElementById('condition').checked
+        const verificationOk = document.getElementById('condition').checked
 
-        if (verification) {
-        document.getElementById("reponse").innerHTML = "Votre formulaire a bien été envoyé.";
-        return false;
+        if (verificationOk) {
+
+            document.getElementById("reponse").innerHTML = "Le formulaire a bien été envoyé.";
+            return false;
         }
 
     }
